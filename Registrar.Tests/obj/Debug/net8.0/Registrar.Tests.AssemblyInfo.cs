885664af09291c504cf4cd8cd03196f4b4cd760a
Christos Maragkos <whitetowersoftware@gmail.com>
2025-08-23 22:24:45 +0300

    Bump version to 1.0.2; update project metadata and nuspec file for release

--- Registrar.Tests/obj/Debug/net8.0/Registrar.Tests.AssemblyInfo.cs
+++ Registrar.Tests/obj/Debug/net8.0/Registrar.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Registrar.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+035612ab150dcc8b921beac9a0c62e7360791ecf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de7f08d678b1aadbab7f05f643c630bfe38c4526")]
 [assembly: System.Reflection.AssemblyProductAttribute("Registrar.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Registrar.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
